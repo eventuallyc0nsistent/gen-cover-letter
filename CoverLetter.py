c9e0109b97918263a479a42c2a8ec8796e9ebc54
Kiran Koduru <crazycreature11@gmail.com>
2013-11-20 19:19:57 -0500

    fix for each row as new pdf

--- CoverLetter.py
+++ CoverLetter.py
@@ -1,7 +1,9 @@
-# Need to read file from the input source : cover_letter.txt , skills.txt
-# Choose the variables from the variables list
-# Find and replace the right variables (#website , #inserttools , #toolproficient , #toolproficientyr)
-# Create PDF
+# Need to read file from the input source : cover_letter.txt , skills.csv
+# cover_letter.txt : the format of your cover letter. Variables in the cover letter are #website , #inserttools , #toolproficient , #toolproficientyr , #company
+# skills.csv : it contains the skills you want to add for the given variables in the apt column
+# Find and replace the right variables
+# Create PDF with CoverLetter-companyName.pdf
+# need to install fpdf module for your python version
 
 from fpdf import FPDF
 import json
@@ -11,16 +13,12 @@ class CoverLetter(object):
 	
 	def __init__(self,cover_letter,skills):
 
-		self.cover_letter = open(cover_letter,'r')
+		self.cover_letter = cover_letter
 		self.skills = skills
 		self.writePDF(self.cover_letter,self.skills)
 
 	# read coverletter and skills csv
 	def writePDF(self,cover_letter,skills):
-
-		self.pdf = FPDF('P','mm','A4') # portrait mode, mm , A4 size paper
-		self.pdf.add_page() # new page added
-		self.pdf.set_font('Arial','',12) # font, Style (B,U,I) , fontsize in pt.
 		
 		# open csv file and read input
 		with open(self.skills) as skills_csv:
@@ -29,22 +27,31 @@ class CoverLetter(object):
 			rownum = 0
 
 			for row in reader:
+
+				self.pdf = FPDF('P','mm','A4') # portrait mode, mm , A4 size paper
+				self.pdf.add_page() # new page added
+				self.pdf.set_font('Arial','',12) # font, Style (B,U,I) , fontsize in pt.
 				
 				#ignore the header row
 				if rownum == 0:
 					pass
 
 				else :
-					for line in self.cover_letter:
+					
+					model_cover_letter = open(self.cover_letter,'r')
+
+					for line in model_cover_letter:
 
 						line = line.replace('#website',row[0])
 						line = line.replace('#inserttools',','.join(row[1].split('#'))) # skills are seperated by '#' split and join them
 						line = line.replace('#toolproficient',row[2])
 						line = line.replace('#toolyr',row[3]) 
 						line = line.replace('#company',row[4])
+
 						self.pdf.write(6,line)
 
 					self.pdf.output('Cover Letter - '+row[4]+'.pdf','F')
+					self.pdf.close()
 				
 				rownum = rownum + 1
 
