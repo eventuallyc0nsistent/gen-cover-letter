9ef5a1586511a051d86df763b007e2dd130512c3
Zackhardtoname <zackL@bu.edu>
2019-08-10 00:15:33 -0400

    Do not add company name in file name

--- gencovletter.py
+++ gencovletter.py
@@ -7,7 +7,7 @@ from ftfy import fix_text
 import time
 import os
 
-def write_cover_letter(company, field, internship, period, prefix):
+def write_cover_letter(company, field, internship, period, fn):
     pdf = FPDF('P', 'mm', 'Letter')  # portrait mode, mm , A4 size paper
     pdf.add_page()  # new blank page
     pdf.set_font('Times', '', 12)  # font, Style (B,U,I) , fontsize in pt.
@@ -24,22 +24,20 @@ def write_cover_letter(company, field, internship, period, prefix):
 
         pdf.write(6, line) #6 is line height
 
-    fullname = prefix + company.replace(" ", "_")
     try:
-        pdf.output(fullname + '.pdf', 'F')
+        pdf.output(fn + '.pdf', 'F')
     except:
         os.system("taskkill /im Acrobat.exe")
         time.sleep(1)
-        pdf.output(fullname + '.pdf', 'F')
+        pdf.output(fn + '.pdf', 'F')
 
     pdf.close()
-    return fullname
 
 if __name__ == "__main__":
     company = "Boston Road Runners"
     field = "data science"
     internship = "Data Analytics Internship"
     period = "the next year"
-    prefix = 'Yi_(Zack)_Zhang_Cover_Letter_'
-    fullname = write_cover_letter(company, field, internship, period, prefix)
-    os.system("start " + fullname + '.pdf')
+    fn = 'Yi_(Zack)_Zhang_Cover_Letter'
+    write_cover_letter(company, field, internship, period, fn)
+    os.system("start " + fn + '.pdf')
